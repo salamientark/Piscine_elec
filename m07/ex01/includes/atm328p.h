884f59c25a92eae72483c1c8d05a3d901207898e
Damien Baladron <dbaladro@made-f0Dr10s8.clusters.42paris.fr>
2025-03-11 12:59:48 +0100

    Finished ex01

---
+++ m07/ex01/includes/atm328p.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atm328p.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbaladro <dbaladro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 12:38:17 by dbaladro          #+#    #+#             */
+/*   Updated: 2025/03/11 12:50:14 by dbaladro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ATM328P_H
+# define ATM328P_H
+
+# include <avr/io.h>
+# include <avr/eeprom.h>
+# include <util/delay.h>
+
+/* ************************************************************************** */
+/*                                   UART                                     */
+/* ************************************************************************** */
+void	uart_init(void);
+uint8_t	uart_rx(void);
+void	uart_tx(const char c);
+void	uart_printstr(const char* str);
+void	uart_printhex(unsigned char c);
+void	uart_printdec(unsigned int nbr);
+uint8_t	uart_read_input(char* buffer, const uint8_t size);
+
+/* ************************************************************************** */
+/*                                  EEPROM                                     */
+/* ************************************************************************** */
+unsigned char	ft_eeprom_read_byte(unsigned int addr);
+void			ft_eeprom_read_block(unsigned char *buffer, unsigned int addr, const unsigned int size);
+void			ft_eeprom_write_byte(unsigned int addr, unsigned char data);
+uint8_t			ft_eeprom_update_byte(unsigned int addr, unsigned char data);
+void			ft_hexdump(void);
+void			ft_hexdiff(uint32_t addr);
+
+#endif
---
+++ m07/ex01/src/main.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbaladro <dbaladro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 16:31:16 by dbaladro          #+#    #+#             */
+/*   Updated: 2025/03/11 12:56:14 by dbaladro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/atm328p.h"
+#include <avr/eeprom.h>
+#include <stdint.h>
+#include <util/delay.h>
+
+
+/** *All the information that I needed for this can be found at:
+ *  - https://ww1.microchip.com/downloads/en/DeviceDoc/ATmega48A-PA-88A-PA-168A-PA-328-P-DS-DS40002061A.pdf
+ *    - Schem at Page 12
+ *    - TIMER0 at Page 102-120
+ *    - Interrupts at page 66 - 78
+ *    - External Interrupts at Pages 79 - 84
+ */
+
+/* ************************************************************************** */
+/*                                    INIT                                    */
+/* ************************************************************************** */
+
+/**
+ * @brief Initialize mmcu
+ */
+void	init(void) {
+	uart_init();
+}
+
+/* ************************************************************************** */
+/*                                   HEXDUMP                                  */
+/* ************************************************************************** */
+
+/**
+ * @brief Convert hex string to number
+ *
+* @param buffer -- buffer to convert
+* @param size -- buffer size
+* @return uint32_t -- converted value
+ */
+uint32_t	str_to_hex(const unsigned char* buffer, uint8_t size) {
+	uint32_t	val = 0;
+
+	for (uint8_t i = 0; i < size; i++) {
+		val <<= 4;
+		if ('0' <= buffer[i] && buffer[i] <= '9')
+			val |= buffer[i] - '0';
+		if ('a' <= buffer[i] && buffer[i] <= 'f')
+			val |= buffer[i] - 'a' + 10;
+		if ('A' <= buffer[i] && buffer[i] <= 'F')
+			val |= buffer[i] - 'A' + 10;
+	}
+	return (val);
+}
+
+/**
+ * @brief Change one byte in eeprom
+ */
+void	change_eeprom_byte(void) {
+	unsigned char	addr_buffer[5] = {0};
+	unsigned char	data_buffer[5] = {0};
+	uint32_t		addr = 0;
+	uint32_t		data = 0;
+	uint8_t			size;
+
+	while (1) {
+		for (uint8_t i = 0; i < 5; i++) {
+			addr_buffer[i] = 0;
+			data_buffer[i] = 0;
+		}
+		uart_printstr("Enter the address to change: ");
+		size = uart_read_input((char *)addr_buffer, 5);
+		addr = str_to_hex(addr_buffer, size);
+		if (addr > 0x03FF) {
+			uart_printstr("Invalid address\r\n\n");
+			continue ;
+		}
+		uart_printstr("Enter the new value: ");
+		size = uart_read_input((char *)data_buffer, 5);
+		data = str_to_hex(data_buffer, size);
+		if (data > 0xFF) {
+			uart_printstr("Invalid value\r\n\n");
+			continue ;
+		}
+		if (ft_eeprom_update_byte(addr, data))
+			ft_hexdiff(addr);
+		else
+			ft_hexdump();
+	}
+}
+
+/* ************************************************************************** */
+/*                                    MAIN                                    */
+/* ************************************************************************** */
+int main() {
+	init();
+
+	while (1) {
+		/* READ TEMP sensor */
+		change_eeprom_byte();
+		_delay_ms(100);
+	}
+
+	return (0);
+}
