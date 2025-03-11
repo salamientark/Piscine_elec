589e6a1df946b54f6113db242dd24565e6ec1c04
Jay Moker <dadamien78@gmail.com>
2025-03-11 22:46:09 +0100

    Updated hexdump

--- m07/ex00/src/main.c
+++ m07/ex00/src/main.c
@@ -6,7 +6,7 @@
 /*   By: dbaladro <dbaladro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 16:31:16 by dbaladro          #+#    #+#             */
-/*   Updated: 2025/03/11 10:58:32 by dbaladro         ###   ########.fr       */
+/*   Updated: 2025/03/11 22:45:56 by dbaladro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,21 @@ void	init(void) {
 	uart_init();
 }
 
+
 /* ************************************************************************** */
 /*                                   EEPROM                                   */
 /* ************************************************************************** */
+
+/**
+ * @brief Copy 16 bytes from src to dest
+ *
+ * @param src -- String to copy
+ * @param dest -- Destination
+ */
+void	buffer_copy(unsigned char *dest, const unsigned char *src) {
+	for(uint8_t i =0; i < 16; i++)
+		dest[i] = src[i];
+}
 
 /**
  * @brief print address in hex format
@@ -53,32 +65,59 @@ void	print_addr(uintptr_t addr, uint8_t width) {
 }
 
 /**
+ * @brief Compare two 16bytes buffer
+ *
+ * @param buff_1 -- first buffer
+ * @param buff_2 -- second buffer
+ * @return 0 if equal, else buff_1 - buff_2
+ */
+uint8_t	buffer_cmp(const unsigned char *buff_1, const unsigned char *buff_2) {
+	for (uint8_t i = 0; i < 16; i++)
+		if (buff_1[i] != buff_2[i])
+			return (buff_1[i] - buff_2[i]);
+	return (0);
+}
+
+/**
  * @brief print one memory line in hexdump format
  *
  * @param addr -- starting addr
  */
-void	print_memory(uintptr_t addr) {
-	unsigned char	mem[16] = {0};
-
-	print_addr(addr, ADDR_WIDTH);
-
-	ft_eeprom_read_block(mem, addr, 16);
+void	print_memory(const unsigned char *data) {
 	for (uint8_t i = 0; i < 16; i++) {
 		if (i % 2 == 0)
 			uart_tx(' ');
-		uart_printhex(mem[i]);
+		uart_printhex(data[i]);
 	}
-	uart_printstr("\r\n");
 }
 
 /**
  * @brief Hexdump of EEPROM
  */
 void	hexdump(void) {
+	unsigned char	last_mem[16] = {0};
+	unsigned char	mem[16] = {0};
+	uint8_t			star = 0;
+	
 	for (uintptr_t i = 0; i < E2END; i+= 16) {
-		print_memory(i);
+		ft_eeprom_read_block(mem, i, 16);
+		if (!buffer_cmp(last_mem, mem)) { /* Same data */
+			if (star)
+				continue ;
+			star = 1;
+			uart_printstr("*\r\n");
+			continue ;
+		}
+
+		print_addr(i, ADDR_WIDTH);
+		print_memory(mem);
+		uart_printstr("\r\n");
+		buffer_copy(last_mem, mem);
+		star = 0;
 		for(volatile uint16_t i = 0; i < 0xFFFF; i++) {}
 	}
+	print_addr(E2END + 1, ADDR_WIDTH);
+	uart_printstr("\r\n");
 }
 
 /* ************************************************************************** */
@@ -90,10 +129,9 @@ int main() {
 	init();
 
 	while (1) {
-		/* READ TEMP sensor */
 		hexdump();
 		uart_printstr("\r\n");
-		for(volatile uint16_t i = 0; i < 0xFFFF; i++) {}
+		for(volatile uint32_t i = 0; i < 0xFFFFF; i++) {}
 	}
 
 	return (0);
